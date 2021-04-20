09d8b90d72cf1f5909309ad0881b9821c2ea680f
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2021-04-21 01:31:02 +0900

    attach components

---
+++ memo-application/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <app-header/>
+    <memo-app/>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader';
+import MemoApp from './components/MemoApp';
+
+export default {
+  name: 'app',
+  components: {
+    // import 한 컴포넌트를 등록한다. 등록된 컴포넌트는 template 영역에서 custome element 로 사용할 수 있다. 
+    AppHeader,
+    MemoApp
+  }
+}
+</script>
+
+<style>
+@import "./styles/reset.css";
+
+#app {
+  width: 560px;
+  margin: 0 auto; 
+}
+
+</style>
---
+++ memo-application/src/components/MemoApp.vue
@@ -0,0 +1,35 @@
+// MemoApp.vue 는 MemoForm[INSERT], Memo[UPDATE/DELETE] 컴포넌트의 부모 컴포넌트 
+// => 부모 컴포넌트에 상태를 초기화한다. 
+<template>
+    <div class="memo-app">
+        <memo-form />
+        <memo/>
+    </div>
+</template>
+
+<script>
+import MemoForm from './MemoForm';
+import Memo from './Memo';
+
+export default {
+    name: 'MemoApp',
+    components: {
+        MemoForm,
+        Memo
+    },
+    data() {
+        return {
+            memos: []
+        }
+    },
+    created(){
+        // 1. 만약 기존에 추가된 localStorage 에 데이터가 있다면 created 훅에서 해당 데이터를 컴포넌트 내의 memos 데이터에 넣어주고, 
+        // 그렇지 않다면 그대로 빈 배열로 초기화한다. 
+        this.memos = localStorage.memos ? JSON.parse(localStorage.memos) : [];
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
